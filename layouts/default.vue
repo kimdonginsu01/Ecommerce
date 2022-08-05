1112a54ea64f3c39dcf4b0ae407cea9fe1249aab
kimdonginsu01 <tuyenminh2k1@gmail.com>
2022-08-05 11:02:44 +0700

    upload code base

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app
+    dark
+    :style="{ backgroundColor: $vuetify.theme.dark ? '#0a0514' : '#fff' }"
+  >
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
